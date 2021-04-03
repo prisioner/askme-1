1f28eba1fa0219234a7577f16b526db2cefbf355
knmrftw <2669997@gmail.com>
2021-04-03 22:53:45 +0300

    Fixes added

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,7 @@
 class Question < ApplicationRecord
-
   belongs_to :user
 
   validates :text, :user, presence: true
   # question text maximum length validation
   validates :text, length: { maximum: 255 }
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   # regexp for username validation
   VALID_USERNAME_REGEXP = /\A\w+\z/
 
+  attr_accessor :password
+
   has_many :questions
 
   validates :email, :username, presence: true
@@ -16,14 +18,28 @@ class User < ApplicationRecord
   validates :username, format: { with: VALID_USERNAME_REGEXP }
   # username maximum length validation
   validates :username, length: { maximum: 40 }
-
-  attr_accessor :password
-
   validates_presence_of :password, on: :create
   validates_confirmation_of :password
 
-  before_validation :username_downcase
+  before_validation :username_downcase, :email_downcase
   before_save :encrypt_password
+
+  def self.authenticate(email, password)
+    user = find_by(email: email) # find person via email
+
+    # we compare the password hashes, not the real passwords! we do not keep password in DB
+    if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(
+      password, user.password_salt, ITERATIONS,
+      DIGEST.length, DIGEST
+    ))
+      user
+    end
+  end
+
+  # binary string to HEX
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
 
   def encrypt_password
     if password.present?
@@ -37,26 +53,17 @@ class User < ApplicationRecord
     end
   end
 
-  # binary string to HEX
-  def self.hash_to_string(password_hash)
-    password_hash.unpack('H*')[0]
-  end
-
-  def self.authenticate(email, password)
-    user = find_by(email: email) # find person via email
-
-    # we compare the password hashes, not the real passwords! we do not keep password in DB
-    if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(
-                                                                    password, user.password_salt, ITERATIONS,
-                                                                    DIGEST.length, DIGEST
-                                                                  ))
-      user
-    end
-  end
-  0
   private
 
   def username_downcase
-    self.username = username.downcase
+    if username.present?
+      self.username = username.downcase
+    end
+  end
+
+  def email_downcase
+    if email.present?
+      self.email = email.downcase
+    end
   end
 end

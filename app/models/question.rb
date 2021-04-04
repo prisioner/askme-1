389a0d101ff1e08bd0049c66b525a85c4b561018
knmrftw <2669997@gmail.com>
2021-04-04 08:57:56 +0300

    48-1 Fixes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
 
-  validates :text, :user, presence: true
+  validates :text, presence: true
   # question text maximum length validation
   validates :text, length: { maximum: 255 }
 end

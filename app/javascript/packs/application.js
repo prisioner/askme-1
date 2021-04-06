d6d19be8fd5e7525e1ce5a5c0466e7a6f5260e16
knmrftw <2669997@gmail.com>
2021-04-06 22:10:20 +0300

    Gemfile edited

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,16 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery"
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
+
+$(function(){
+  $('#ask-button').click(function(){
+    $('#ask-form').slideToggle(300);
+    return false;
+  });
+});
